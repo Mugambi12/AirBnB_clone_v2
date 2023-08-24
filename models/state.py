4215271ddba8c48dbbb775406c03c29453fe5f2c
mugambi12 <silasmungiria16@gmail.com>
2023-08-24 05:11:28 +0300

    error correction updates

--- models/state.py
+++ models/state.py
@@ -8,7 +8,6 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import shlex
 
 
 class State(BaseModel, Base):
@@ -41,20 +40,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-        else:
-            def cities(self):
-                """
-                Getter for a list of city instances related to the state.
-                """
-                var = models.storage.all()
-                lista = []
-                result = []
-                for key in var:
-                    city = key.replace('.', ' ')
-                    city = shlex.split(city)
-                    if (city[0] == 'City'):
-                        lista.append(var[key])
-                for elem in lista:
-                    if (elem.state_id == self.id):
-                        result.append(elem)
-                return (result)
