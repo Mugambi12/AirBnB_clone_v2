1b99c04cc9e9702096c8d1751eee614986902e7b
mugambi12 <silasmungiria16@gmail.com>
2023-08-24 04:42:17 +0300

    error correction updates

--- models/user.py
+++ models/user.py
@@ -13,32 +13,19 @@ from models.review import Review
 
 
 class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    Representation of the User class.
-    """
-    if models.selected_storage == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
-        reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="user")
-
-    else:
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = []
-        reviews = []
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes an instance of the User class.
-        """
-        super().__init__(*args, **kwargs)
-
-        if self.password and models.selected_storage == 'db':
-            hashed_password = hashlib.md5(self.password.encode("utf-8"))
-            self.password = hashed_password.hexdigest()
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
