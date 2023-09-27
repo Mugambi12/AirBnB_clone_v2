564fcfd4807a982ce13f2b47b34656a30557332d
mugambi12 <silasmungiria16@gmail.com>
2023-09-28 00:55:22 +0300

    web static setup update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,56 +1,38 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# script that sets up your web servers for deployment of web_static
 
-# Check if Nginx is already installed
-if ! command -v nginx &> /dev/null
-then
-    apt-get update
-    apt-get install -y nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create directories if they don't exist
-directories=("/data/web_static/releases/test/" "/data/web_static/shared/")
-for dir in "${directories[@]}"
-do
-    mkdir -p "$dir"
-done
-
-# Create fake HTML file
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-
-# Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership and group recursively
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-# Update Nginx configuration
-nginx_config="/etc/nginx/sites-available/web_static"
-if [ ! -f "$nginx_config" ]; then
-    printf %s "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        add_header X-Served-By $HOSTNAME;
-        root   /var/www/html;
-        index  index.html index.htm;
-        location /hbnb_static {
-            alias /data/web_static/current;
-            index index.html index.htm;
-        }
-        error_page 404 /404.html;
-        location /404 {
-          root /var/www/html;
-          internal;
-        }
-    }" > "$nginx_config"
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-    # Create symbolic link to enable the site
-    ln -s "$nginx_config" "/etc/nginx/sites-enabled/"
-fi
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Restart Nginx service
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
 service nginx restart
-
-# Exit successfully
-exit 0
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
-import os
-from datetime import datetime
-from fabric.api import local, runs_once
+#!/usr/bin/env bash
+# script that sets up your web servers for deployment of web_static
 
+apt-get update
+apt-get install -y nginx
 
-@runs_once
-def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format
-    (
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-    except Exception:
-        output = None
-    return output
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
