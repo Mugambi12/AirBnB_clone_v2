5811dccffb68e8087f713aeffef7e6be0246f0e1
mugambi12 <silasmungiria16@gmail.com>
2023-09-28 02:42:58 +0300

    web static setup update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,34 @@
-#!/usr/bin/env bash
-# script that sets up your web servers for deployment of web_static
+#!/bin/bash
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx if not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo chown -R ubuntu:ubuntu /data/
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create or recreate the symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i '/^\tlocation \/ {/a\
+\t\talias /data/web_static/current/;' "$config_file"
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Restart Nginx
+sudo service nginx restart
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+exit 0
