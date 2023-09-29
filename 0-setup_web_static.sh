6ab6cb48a2b6851c64620663d6e43e7d2769a4f8
mugambi12 <silasmungiria16@gmail.com>
2023-09-29 17:33:30 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,32 +4,46 @@
 apt-get update
 apt-get install -y nginx
 
+# Create necessary directories
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
+
+# Create a basic index.html file
 echo "Holberton School" > /data/web_static/releases/test/index.html
+
+# Create a symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Set ownership and group
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-printf %s "server {
+# Configure Nginx
+printf %s "
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 http://cuberule.com/;
     }
+
     error_page 404 /404.html;
+
     location /404 {
       root /var/www/html;
       internal;
     }
-}" > /etc/nginx/sites-available/default
+}
+" > /etc/nginx/sites-available/default
 
+# Restart Nginx
 service nginx restart
