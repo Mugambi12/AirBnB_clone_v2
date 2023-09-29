f3c641bac5bcad1aa8b47b6dccbe824a23cf47f5
mugambi12 <silasmungiria16@gmail.com>
2023-09-29 17:42:36 +0300

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,38 +1,36 @@
-#!/usr/bin/env bash
-# script that sets up your web servers for deployment of web_static
+#!/usr/bin/python3
+"""
+A module for Fabric script that generates a .tgz archive.
+"""
 
-apt-get update
-apt-get install -y nginx
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+@runs_once
+def do_pack():
+    """
+    Archives the static files.
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+    return output
