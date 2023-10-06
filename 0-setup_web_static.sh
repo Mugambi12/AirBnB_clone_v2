db1b1fc62606a723fb616a78996821d86d73c2d0
mugambi12 <silasmungiria16@gmail.com>
2023-10-06 20:22:46 +0300

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 # Set up server file system for deployment
 
-# install nginx
+# Update and install nginx if not already installed
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo service nginx start
 
-# configure file system
+# Check if nginx is running, and start it if not
+if ! sudo service nginx status | grep -q "active (running)"; then
+    sudo service nginx start
+fi
+
+# Create directories if they don't exist
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# set permissions
+# Create index.html with content
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
-# configure nginx
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+# Check if the symbolic link already exists, and remove it if it does
+if [ -L /data/web_static/current ]; then
+    sudo rm /data/web_static/current
+fi
 
-# restart web server
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Check if the location block already exists in nginx config, and add if it doesn't
+if ! sudo grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
+    sudo sed -i '44i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+fi
+
+# Restart web server
 sudo service nginx restart
