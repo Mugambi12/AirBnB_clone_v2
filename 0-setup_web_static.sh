ef0e83c1d26fcb846f278af2c29669fb6db28677
mugambi12 <silasmungiria16@gmail.com>
2023-10-29 14:08:17 +0300

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
+# Set up server file system for deployment
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+# Update and install nginx if not already installed
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+# Check if nginx is running, and start it if not
+if ! sudo service nginx status | grep -q "active (running)"; then
+    sudo service nginx start
 fi
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+# Create directories if they don't exist
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-# Create a fake HTML file
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+# Create index.html with content
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to ubuntu user and group
+# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/^server {/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' $config_file
+# Check if the symbolic link already exists, and remove it if it does
+if [ -L /data/web_static/current ]; then
+    sudo rm /data/web_static/current
+fi
 
-# Restart Nginx
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Check if the location block already exists in nginx config, and add if it doesn't
+if ! sudo grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
+    sudo sed -i '44i \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+fi
+
+# Restart web server
 sudo service nginx restart
