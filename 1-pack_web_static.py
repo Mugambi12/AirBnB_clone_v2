64770ebc77918b16b620d39bc07c8509e43cd645
mugambi12 <silasmungiria16@gmail.com>
2023-10-29 15:10:00 +0300

    updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,23 @@
-import subprocess
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
 
-def run_fabric_script():
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """generates a tgz archive"""
     try:
-        result = subprocess.run(['fab', '-f', '1-pack_web_static.py', 'do_pack'], capture_output=True, text=True)
-        print(result.stdout)
-        print(result.stderr)
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
-run_fabric_script()
+        return None
